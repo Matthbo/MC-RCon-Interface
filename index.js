3a2f2ce2412efb6e42490dcbe3c6c4d2cdc18040
Matthijs <matthijs.booman@hotmail.com>
2017-03-13 23:07:57 +0100

    added express as depend

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-var Rcon = require('./rcon');
+var Rcon = require('./rcon'),
+	express = require('express');
 
 var server1 = new Rcon('127.0.0.1', 25575, 'kaas').connect();
+
+var mainWebServer = express();
+
+mainWebServer.listen(443, function(){
+	console.log("Main webserver on port 443");
+});
+
+var adminPanelServer = express();
+
+adminPanelServer.listen(1337, function(){
+	console.log("Admin webserver on port 1337");
+});
